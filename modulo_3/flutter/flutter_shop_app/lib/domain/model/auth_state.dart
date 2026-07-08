11cd6dd04c847c4276014674e00b9af2121d3504
AlexLopezF04 <j.alexander.lopez.f@gmail.com>
2026-07-07 19:33:31 -0500

    SHOPAPP CHECKPOINT 03/07-07-2026

--- modulo_3/flutter/flutter_shop_app/lib/domain/model/auth_state.dart
+++ modulo_3/flutter/flutter_shop_app/lib/domain/model/auth_state.dart
@@ -1,22 +1,39 @@
 import 'auth_models.dart';
 
-enum AuthStatus { initial, loading, authenticated, unauthenticated, error }
+enum AuthStatus { checking, authenticated, unauthenticated }
 
 class AuthState {
   final AuthStatus status;
   final LoggedUser? user;
-  final String? error;
+  final String?     error;
 
   const AuthState({
-    this.status = AuthStatus.initial,
+    required this.status,
     this.user,
     this.error,
   });
 
-  AuthState copyWith({AuthStatus? status, LoggedUser? user, String? error}) =>
-      AuthState(
-        status: status ?? this.status,
-        user: user ?? this.user,
-        error: error,
-      );
+  const AuthState.checking()
+      : status = AuthStatus.checking,
+        user   = null,
+        error  = null;
+
+  const AuthState.authenticated(this.user)
+      : status = AuthStatus.authenticated,
+        error  = null;
+
+  const AuthState.unauthenticated([this.error])
+      : status = AuthStatus.unauthenticated,
+        user   = null;
+
+  bool get isAuthenticated   => status == AuthStatus.authenticated;
+  bool get isChecking        => status == AuthStatus.checking;
+  bool get isStaff           => user?.isStaff ?? false;
+  bool get isUnauthenticated => status == AuthStatus.unauthenticated;
+
+  AuthState copyWith({AuthStatus? status, LoggedUser? user, String? error}) => AuthState(
+    status: status ?? this.status,
+    user:   user   ?? this.user,
+    error:  error,
+  );
 }
